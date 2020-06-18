0cca2bdc3685b844f66d82af0a1a45449e463bea
C55233 <francesco.pizzo@nl.abnamro.com>
2020-06-18 11:42:55 +0200

    test with @EnableCosmosRepositories

--- src/main/java/com/github/paizo/cosmos/CosmosConfiguration.java
+++ src/main/java/com/github/paizo/cosmos/CosmosConfiguration.java
@@ -1,57 +1,11 @@
 package com.github.paizo.cosmos;
 
-import com.azure.data.cosmos.CosmosKeyCredential;
-import com.microsoft.azure.spring.data.cosmosdb.config.CosmosDBConfig;
-import com.microsoft.azure.spring.data.cosmosdb.core.ResponseDiagnostics;
-import com.microsoft.azure.spring.data.cosmosdb.core.ResponseDiagnosticsProcessor;
+import com.microsoft.azure.spring.data.cosmosdb.repository.config.EnableCosmosRepositories;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-import javax.annotation.Nullable;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-//@Configuration
-//@EnableCosmosRepositories
-public class CosmosConfiguration { //} extends AbstractCosmosConfiguration {
-
-    @Value("${azure.cosmosdb.uri}")
-    private String uri;
-
-    @Value("${azure.cosmosdb.key}")
-    private String key;
-
-    @Value("${azure.cosmosdb.secondaryKey}")
-    private String secondaryKey;
-
-    @Value("${azure.cosmosdb.database}")
-    private String dbName;
-
-    @Value("${azure.cosmosdb.populateQueryMetrics}")
-    private boolean populateQueryMetrics;
-
-    private CosmosKeyCredential cosmosKeyCredential;
-
-    private static class ResponseDiagnosticsProcessorImplementation implements ResponseDiagnosticsProcessor {
-
-        @Override
-        public void processResponseDiagnostics(@Nullable ResponseDiagnostics responseDiagnostics) {
-            log.info("Response Diagnostics {}", responseDiagnostics);
-        }
-    }
-
-    @Bean
-    @Primary
-    public CosmosDBConfig getConfig() {
-        this.cosmosKeyCredential = new CosmosKeyCredential(key);
-        CosmosDBConfig cosmosdbConfig = CosmosDBConfig.builder(uri, this.cosmosKeyCredential, dbName).build();
-        cosmosdbConfig.setPopulateQueryMetrics(populateQueryMetrics);
-        cosmosdbConfig.setResponseDiagnosticsProcessor(new ResponseDiagnosticsProcessorImplementation());
-        return cosmosdbConfig;
-    }
-
-    public void switchToSecondaryKey() {
-        this.cosmosKeyCredential.key(secondaryKey);
-    }
+@Configuration
+@EnableCosmosRepositories
+public class CosmosConfiguration {
 }
